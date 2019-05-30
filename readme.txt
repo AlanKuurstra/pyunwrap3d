af8c0ce00d993673c1d66a5a25298e5d3a71fd9b
akuurstr <akuurstr@uwo.ca>
2019-05-30 11:32:18 -0400

    first commit

---
+++ readme.txt
@@ -0,0 +1,9 @@
+note automatic unit testing is not yet implemented through a gitlab runner.
+
+#to build and install the module
+python setup.py build
+to install locally:
+python setup.py install --install-platlib .
+to install to standard locations:
+python setup.py install
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup, Extension
+
+import numpy
+import os
+
+extensions = []
+
+includeDirs = numpy.get_include()
+libDirs = "" #giving a blank lib list causes errors!
+libs = ""
+
+extensions.append(
+    Extension('Unwrap3d',
+              sources=['unwrap3dInterface.cpp'],
+              include_dirs=[includeDirs],
+              #library_dirs=[libDirs], giving a blank lib list causes errors!
+              #libraries=libs, giving a blank lib list causes errors!
+              #extra_compile_args=[]
+              )
+    )
+
+setup(
+    name="Unwrap3d",
+    version="0.1",
+    description = "Python interface to unwrap3d",
+    #packages=['PfileInterface'],
+    ext_modules=extensions)
