f27301edcd54b69491bd1b03290b2f77c5f6d7c8
akuurstr <akuurstr@uwo.ca>
2019-06-03 11:40:17 -0400

    modified unit_test to call setup inside interpreter

--- test/run_before_unit_test.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ..
-python -c "import sys;print(sys.version)"
-python setup.py build
-python setup.py install --install-platlib test/.
--- test/unit_test.py
+++ test/unit_test.py
@@ -4,27 +4,40 @@ Created on Mon Jan 25 16:08:32 2016
 
 @author: Alan
 """
+#make the module and install it in the test directory
+from distutils.core import run_setup
+import os
+import glob
+from shutil import copyfile
+
+old_dir = os.getcwd()
+os.chdir('..')
+run_setup('setup.py',script_args=['build'])
+lib_file = os.path.abspath(glob.glob('build/*lib*/Unwrap3d*')[0])
+os.chdir(old_dir)
+copyfile(lib_file,os.path.basename(lib_file))
+
+import Unwrap3d
+import os
 import nibabel as nib
 import numpy as np
-import Unwrap3d
-from subprocess import call
 
-all(['bash', 'run_before_unit_test.bash'])
-good_result = np.load('unwrapped_result.npy')
+fdir=os.path.dirname(__file__)
+good_result = np.load(os.path.join(fdir,'unwrapped_result.npy'))
 
-img = nib.load('test_img.nii.gz').get_data()
-mask=nib.load('test_img_mask.nii.gz').get_data()
+img = nib.load(os.path.join(fdir,'test_img.nii.gz')).get_data()
+mask=nib.load(os.path.join(fdir,'test_img_mask.nii.gz')).get_data()
 
 img=np.asfortranarray(img,'float32')
 mask=np.asfortranarray(mask,'bool')
 result = Unwrap3d.unwrap3d(img,mask)
-assert (result==good_result).all() 'algorithm failed with fortran style array'
+assert (result==good_result).all(), 'algorithm failed with fortran style array'
 
 
 img = np.ascontiguousarray(img,'float32')
 mask=np.ascontiguousarray(mask,'bool')
 result = Unwrap3d.unwrap3d(img,mask)
-assert (result==good_result).all() 'algorithm failed with c style array'
+assert (result==good_result).all(), 'algorithm failed with c style array'
 
 #from vidi3d import compare3d
 #compare3d((img,result))
